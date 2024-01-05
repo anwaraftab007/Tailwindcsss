da1eef8236db496cf3484bbd6c32c7048465ae6f
anwaraftab007 <anwaraftab@gmail.com>
2024-01-05 04:16:51 -0800

    [001] Project Committed

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+const App = () => {
+  return (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r oadding-b">
+      Hero
+    </section>  
+    <section className="padding">
+      PopularProduct
+    </section>  
+    <section className="padding">
+      SuperQuality
+    </section>  
+    <section className="padding-x py-10">
+      Services
+    </section>  
+    <section className="padding">
+      SpecialOffers
+    </section>  
+    <section className="bg-blue-500/10 padding">
+      CustomerReviews
+    </section>  
+    <section className="padding-x sm:py-32 py-16 w-full">
+      Subscribe
+    </section>  
+    <section className="bg-black padding-x pading-t pb-8">
+      Footer
+    </section>
+  </main>
+  );
+}
+
+export default App;
