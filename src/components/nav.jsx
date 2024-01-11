ec55b8cb06aec9e8b7ef6c97411f38b5fca0df8f
anwaraftab007 <anwaraftab@gmail.com>
2024-01-11 12:39:17 -0800

    [006] Project Bug{001} Navbar Updated

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,11 +1,37 @@
 import { hamburger } from "../assets/icons"
 import { navLinks } from "../constants"
 import { headerLogo } from "../assets/images"
+import { useState } from 'react'
+
+
+
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+  };
+  const Menu = ({isOpen}) => {
+    return (
+      <div className="container relative m-auto p-3 flex justify-between items-center max-lg:top-9">
+      <ul className={`lg:flex-1 lg:flex lg:justify-center items-center gap-16 max-lg:grid max-lg:grid-cols-1 max-lg:bg-red-500 max-lg:text-white max-lg:p-7`}>
+    {navLinks.map((item) => (
+      <li key={item.label}>
+        <a href={item.href}
+        className="block max-lg:inline-block font-montserrat leading-normal text-lg text-slate-gray">
+          {item.label}
+        </a>
+      </li>
+    ))}
+  </ul></div>
+    
+    )
+  }
   return (
     <header className="padding-x py-8 absolute z-10 w-full">
-        <nav className="flex justify-between items-center max-container">
-          <a href="#">
+        <nav className="max-lg:relative flex justify-between max-lg:justify-end items-center max-container">
+          <a className="max-lg:absolute max-lg:left-0 max-lg:top-1" href="#">
            <img src={headerLogo} alt="Nike logo" width={130} height={29}/>
           </a>
           <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
@@ -18,13 +44,28 @@ const Nav = () => {
               </li>   
             ))}
           </ul>
-          <div className="hidden max-lg:block">
-            <img 
-            src={hamburger}
-            alt="Hamburger"
-            width={25} 
-            height={25} />
-          </div>
+          <div className="">
+          <button
+         onClick={toggleNavbar}
+         className="flex items-center px-3 py-2 rounded-full text-black-500 hover:text-black-400 max-lg:absolute max-lg:right-0 max-lg:top-1 lg:hidden" >
+         <svg
+           className={`fill-current h-3 w-3 ${isOpen ? "hidden" : "block"}`}
+           viewBox="0 0 20 20"
+           xmlns="http://www.w3.org/2000/svg"
+           style={{ width: "24", height: "24" }}>
+           <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+         </svg>
+         <svg
+           className={`fill-current h-3 w-3 ${isOpen ? "block" : "hidden"}`}
+           viewBox="0 0 20 20"
+           xmlns="http://www.w3.org/2000/svg" style={{ width: "24", height: "24" }}
+         >
+           <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
+         </svg>
+       </button>
+        {isOpen && <Menu isOpen={isOpen}/> }
+      </div>
+     
         </nav>
     </header>
   )
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -8,7 +8,7 @@ import ShoeCard from "../components/ShoeCard"
 const Hero = () => {
   const [bigShoeImg, setbigShoeImg] = useState(bigShoe1)
   return (
-    <sectin id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container ">
+    <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container ">
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
         <p className="text-xl font-montserrat text-red-400">Our Summer Collecton</p>
         <h1 className="mt-10 font-palanquin text-8xl font-bold ">
@@ -42,7 +42,7 @@ const Hero = () => {
       </div>
       
       </div>
-    </sectin>
+    </section>
   )
 }
 
